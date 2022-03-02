4ddbfd9b3a3dd6e6661dcdc174da71a0f8925803
丁雨健 <365409156@qq.com>
2022-03-02 09:49:14 +0800

    完成提交订单页面的布局

---
+++ pages/payment-page/payment.vue
@@ -0,0 +1,217 @@
+<template>
+    <view>
+        <view class="payment-view">
+            <!-- 收货地址 -->
+            <view class="payment-name" @click="shAddress()">
+                <view class="payment-left-img">
+                    <image src="/static/loading/address-shouhuo.svg" mode="widthFix"></image>
+                </view>
+             	  <!-- 收货地址 -->
+              	 <!-- <view class="payment-add" v-if="nameadd">
+            		<text>{{address.name}}</text>
+            		<text>{{address.mobile}}</text>
+            		<text>{{address.city}}{{address.address}}</text>
+            	</view> -->
+                <view class="payment-add">请选择收货地址</view>
+                <view class="payment-right-img">
+                    <image src="/static/loading/shouhuo-jiantou.svg" mode="widthFix"></image>
+                </view>
+            </view>
+            <!-- 商品详情 -->
+            <view class="payment-commodity">
+                <!-- <block v-for="(item,index) in comminfo" :key="index"> -->
+                    <view class="payment-order">
+                        <view class="payment-order-img">
+                            <image src="http://h.thexxdd.cn/tianmao/public%5Cuploads%5C1603102805188-9447673.jpg" mode="aspectFill"></image>
+                        </view>
+                        <view class="payment-title">
+                            <text>标题</text>
+                            <text class="text-you">颜色:黑色;尺码:S</text>
+                        </view>
+                        <view class="payment-flex">
+                            <view class="payment-price">
+                                <text>￥ 100</text>
+                                <text class="text-you">x1</text>
+                            </view>
+                            <view>
+                                共1件 小计: ￥ 100
+                            </view>
+                        </view>
+                    </view>
+                <!-- </block> -->
+            </view>
+        </view>
+        <!-- 底部提交订单 -->
+        <view class="place-order">
+            <text>合计: ￥ 100</text>
+            <text @click="placeOrder()">提交订单</text>
+        </view>
+    </view>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                // 上个页面接收的商品数据
+                comminfo: [],
+                Totalprice: 0,
+                // 收货地址数据
+                address: {},
+                // 存储购物车的商品id
+                idcard: [],
+                // 存储商户订单号和订单id
+                outno: '',
+                ide: ''
+            }
+        },
+
+        onLoad(e) {
+            console.log(JSON.parse(e.cartdata))
+        },
+    }
+</script>
+
+<style>
+    page {
+        background-color: #f2f2f2;
+    }
+
+    .payment-view {
+        margin: 15upx;
+    }
+
+    .payment-name {
+        height: 150upx;
+        background-color: #FFFFFF;
+        border-radius: 15upx;
+        font-size: 28upx;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0 10upx;
+    }
+
+    .payment-left-img {
+        width: 60upx;
+        height: 60upx;
+    }
+
+    .payment-left-img image {
+        width: 60upx;
+        height: 60upx;
+    }
+
+    .payment-add text {
+        display: block;
+    }
+
+    .payment-add text:nth-child(2) {
+        padding: 4rpx 0;
+    }
+
+    .payment-add {
+        flex: 1;
+        padding-left: 20rpx;
+    }
+
+    .payment-right-img {
+        width: 40upx;
+        height: 40upx;
+    }
+
+    .payment-right-img image {
+        width: 40upx;
+        height: 40upx;
+    }
+
+    /* 下单商品 */
+    .payment-commodity {
+        background-color: #FFFFFF;
+        border-radius: 15upx;
+        font-size: 28upx;
+        padding: 10rpx;
+        margin: 15upx 0;
+    }
+
+    .payment-order {
+        display: flex;
+        justify-content: space-between;
+        height: 200upx;
+        /* background: #4CD964; */
+        margin-bottom: 20rpx;
+    }
+
+    .payment-order text {
+        display: block;
+    }
+
+    .payment-order-img {
+        width: 200upx;
+        height: 200upx;
+        border-radius: 8upx;
+        padding-right: 10upx;
+    }
+
+    .payment-order-img image {
+        width: 200upx;
+        height: 200upx;
+        border-radius: 8upx;
+    }
+
+    .payment-title {
+        flex: 1;
+    }
+
+    .payment-title text:nth-child(1) {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+    }
+
+    .payment-title text:nth-child(2) {
+        background-color: rgb(250, 250, 250);
+        padding: 5upx;
+        border-radius: 7upx;
+    }
+
+    .payment-price {
+        text-align: right;
+    }
+
+    .payment-flex {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    .text-you {
+        color: rgb(156, 156, 156);
+        margin-top: 8upx;
+    }
+
+    /* 提交 */
+    .place-order {
+        height: 90upx;
+        background-color: #FFFFFF;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+    }
+
+    .place-order text:nth-child(2) {
+        background-color: rgb(255, 0, 54);
+        height: 90upx;
+        width: 270upx;
+        color: #FFFFFF;
+        text-align: center;
+        line-height: 90upx;
+        margin-left: 30upx;
+    }
+</style>
---
+++ unpackage/dist/dev/mp-weixin/pages/order-details/order.js
@@ -0,0 +1,20 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/order-details/order"],{
+
+/***/ 218:
+/*!*************************************************************************!*\
+  !*** D:/天猫小程序/tianmao/main.js?{"page":"pages%2Forder-details%2Forder"} ***!
+  \*************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 5);
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 3));
+var _order = _interopRequireDefault(__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module './pages/order-details/order.vue'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;
+createPage(_order.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ })
+
+},[[218,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/order-details/order.js.map
